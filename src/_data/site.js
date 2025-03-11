bca1f61683575316786aa046f36289fde9c39d4e
cohnon <me@cohnons-MacBook-Air.local>
2025-03-11 11:10:53 -0700

    added photos and tiny cleanup

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,11 +1,10 @@
 'use strict'
 
-module.exports = {
-  author: 'cohnon',
-  source: 'https://github.com/cohnon/portfolio',
-  lastUpdated: (new Date()).toDateString(),
-  contact: {
-    email: 'me@cohnon.com',
-    discord: 'cohnon',
-  },
+export default {
+    source: 'https://github.com/cohnon/cohnon.github.io',
+    lastUpdated: (new Date()).toDateString(),
+    contact: {
+        email: 'me@cohnon.com',
+        discord: 'cohnon',
+    },
 };
